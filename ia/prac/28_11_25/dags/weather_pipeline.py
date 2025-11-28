e60ca59dd678f8d3ad5f3bc41ac3c582dc418fec
poslam <kirden04@gmail.com>
2025-11-28 18:57:43 +1000

    upd

--- ia/prac/28_11_25/dags/weather_pipeline.py
+++ ia/prac/28_11_25/dags/weather_pipeline.py
@@ -1,22 +1,38 @@
+import logging
 import os
 from datetime import datetime, timedelta
 
 import pandas as pd
 import requests
-from airflow.models import Variable
+from airflow.hooks.base import BaseHook
 from airflow.sdk import dag, task
 
-DATA_PATH = "/Users/poslam/Downloads/projects/fefu/7/ds/ia/prac/28_11_25/data/weather.csv"
+DATA_PATH = "/opt/airflow/logs/weather.csv"
 CITIES = {
     "vdk": (43.1332, 131.9),
     "khv": (48.4814, 135.0721),
 }
 
+os.makedirs(
+    os.path.dirname("/opt/airflow/logs/weather_pipeline.log"), exist_ok=True
+)
+
+logger = logging.getLogger("weather_pipeline")
+handler = logging.FileHandler("/opt/airflow/logs/weather_pipeline.log")
+formatter = logging.Formatter(
+    "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+logger.setLevel(logging.INFO)
+
+logger.setLevel(logging.DEBUG)
+
 default_args = {
     "owner": "airflow",
     "depends_on_past": False,
-    "retries": 1,
-    "retry_delay": timedelta(minutes=1),
+    "retries": 0,
+    "retry_delay": timedelta(seconds=1),
 }
 
 
@@ -30,7 +46,12 @@ default_args = {
 def weather_pipeline():
     @task()
     def fetch_weather(lat: float, lon: float) -> dict:
-        api_key = Variable.get("OPENWEATHER_API_KEY")
+        logger = logging.getLogger(__name__)
+        logger.info("start: get info")
+
+        conn = BaseHook.get_connection("openweather-api")
+        api_key = conn.extra_dejson.get("api-key")
+
         resp = requests.get(
             "https://api.openweathermap.org/data/2.5/weather",
             params={
@@ -41,27 +62,45 @@ def weather_pipeline():
             timeout=15,
         )
         resp.raise_for_status()
+
+        logger.info("end: get info")
+
         return resp.json()
 
     @task()
     def parse_data(raw: dict) -> dict:
-        return {
-            "temp": raw["main"].get("temp"),
-            "temp_min": raw["main"].get("temp_min"),
-            "temp_max": raw["main"].get("temp_max"),
-            "feels_like": raw["main"].get("feels_like"),
-            "pressure": raw["main"].get("pressure"),
-            "humidity": raw["main"].get("humidity"),
-            "wind_speed": raw.get("wind", {}).get("speed"),
-            "dt": raw.get("dt"),
-            "weather": raw.get("weather", [{}])[0].get("main"),
-        }
+        logger = logging.getLogger(__name__)
+        logger.info("start: parse data")
+        try:
+            data = {
+                "temp": raw["main"].get("temp"),
+                "temp_min": raw["main"].get("temp_min"),
+                "temp_max": raw["main"].get("temp_max"),
+                "feels_like": raw["main"].get("feels_like"),
+                "pressure": raw["main"].get("pressure"),
+                "humidity": raw["main"].get("humidity"),
+                "wind_speed": raw.get("wind", {}).get("speed"),
+                "dt": raw.get("dt"),
+                "weather": raw.get("weather", [{}])[0].get("main"),
+            }
+        except Exception as e:  
+            logger.error(f"error: parse data - {str(e)}")
+
+        logger.info("end: parse data")
+
+        return data
 
     @task()
     def prep_data(parsed: dict) -> dict:
+        logger = logging.getLogger(__name__)
+        logger.info("start: prep")
         for key in ("temp", "temp_min", "temp_max", "feels_like"):
             if parsed.get(key) is not None:
-                parsed[key] = parsed[key] - 273.15
+                parsed[key] = round(parsed[key] - 273.15, 2)
+
+        for key in "wind_speed":
+            if parsed.get(key) is not None:
+                parsed[key] = round(parsed[key], 2)
 
         dt = datetime.fromtimestamp(parsed.pop("dt"))
         parsed.update(
@@ -72,16 +111,22 @@ def weather_pipeline():
                 "hour": dt.hour,
             }
         )
+        logger.info("end: prep")
         return parsed
 
     @task()
     def write_data(row: dict) -> str:
+        logger = logging.getLogger(__name__)
+        logger.info("start: data save")
+
         os.makedirs(os.path.dirname(DATA_PATH), exist_ok=True)
         df = pd.DataFrame([row])
         if os.path.exists(DATA_PATH):
             df.to_csv(DATA_PATH, mode="a", header=False, index=False)
         else:
             df.to_csv(DATA_PATH, index=False)
+
+        logger.info("end: data save - updated df saved to: %s", DATA_PATH)
 
         return DATA_PATH
 
